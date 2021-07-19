37ed490456aceb1f9129d927f31257883b4e6de0
praveenmskr <praveenkumar14161@gmail.com>
2021-07-19 16:08:18 +0530

    Done

---
+++ src/org/phone/EternalStorage.java
@@ -0,0 +1,13 @@
+package org.phone;
+
+public class EternalStorage {
+	  public static void main(String[] args) {
+		  int i=67;
+		  if (i%2==0) {
+			System.out.println("Number is even");
+		} else {
+			System.out.println("Number is odd");
+
+		}
+	  }
+}
